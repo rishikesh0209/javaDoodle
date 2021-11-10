264b8e49ca65cdd7fce6cdae533d56ac64a985a8
rishikesh0209 <rishikesh.shetty@zoho.com>
2021-11-10 13:46:25 +0530

    first java code

---
+++ src/com/javaWithRishi/Main.java
@@ -0,0 +1,10 @@
+package com.javaWithRishi;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello World !");
+    }
+}
